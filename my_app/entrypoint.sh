c287fa4da4df8eb260ddd6ffa7df3856cd13e057
hottabych <varvar1987a@mail.ru>
2024-08-08 14:11:59 +0300

    STATIC

--- my_app/entrypoint.sh
+++ my_app/entrypoint.sh
@@ -17,5 +17,5 @@ fi
 # Выполняем миграции
 python manage.py makemigrations
 python manage.py migrate
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 exec "$@"
